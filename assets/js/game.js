ef4c2700b891302553058f237931f2f9f6454c08
Richard <richardyjkim@gmail.com>
2021-02-10 12:36:19 -0800

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,8 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+// "LOSE" - Player robot's health is zero or less
+//      * Fight all enemy robots
+//      * Defeat each enemt robot
 // let playerName = 'Lucid';
 let playerName = window.prompt("what is your robot's name?");
 let playerHealth = 100;
@@ -8,25 +13,38 @@ console.log(playerMoney);
 console.log(playerName, playerAttack, playerHealth);
 console.log("You can put regular sentences in these.");
 
-let enemyName = "Roberto";
+let enemyNames = ["Roberto", "Amy  Android", "Robo Trumble"];
+    console.log (enemyNames);
+    console.log (enemyNames[0]);
+    console.log (enemyNames[1]);
+    console.log (enemyNames[2]);
+    console.log (enemyNames.length);
+for (let i = 0; i < enemyNames.length ; i++) {
+    console.log (enemyNames[i]);
+    console.log (i);
+    console.log (enemyNames[i] + " is at " + i + " index");
+}
 let enemyHealth = 50;
 let enemyAttack = 12;
 
 // fight function
-let fight = function() {
+let fight = function(enemyName) {
+
     // Altert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
     // ask player if they'd like to fight or skip
-    let promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'Fight' or 'SKIP' to choose.");
+    let promptFight = window.prompt(
+        "Would you like to FIGHT or SKIP this battle? Enter 'Fight' or 'SKIP' to choose."
+    );
 
      // if player choses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
-    // remove enemy's health by substracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
-    console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
-    );
+        // remove enemy's health by substracting the amount set in the playerAttack variable
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(
+            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
+        );
 
     // check enemy's health
     if (enemyHealth <= 0) {
@@ -68,5 +86,8 @@ let fight = function() {
 }
 };
 
+
 // run fight funtion to start game
-    fight();
+    for(let i = 0; i < enemyNames.length; i++ ) {
+        fight(enemyNames[i]);
+    }
